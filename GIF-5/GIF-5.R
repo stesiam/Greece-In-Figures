495c283983fe72701ebe59b4f5407744b461039f
stesiam <52320488+stesiam@users.noreply.github.com>
2024-09-30 18:15:58 +0300

    Update GIF-6 viz

--- GIF-5/GIF-5.R
+++ GIF-5/GIF-5.R
@@ -1,68 +1,306 @@
-## Libraries
+library(readr)
+library(readxl)
+library(dplyr)
+library(fuzzyjoin)
+library(qpdf)
+library(tabulizer)
 
 library(ggplot2)
+library(MetBrewer)
 library(sf)
+library(glue)
 library(ggtext)
 library(showtext)
+library(sysfonts)
 
-## Add fonts
 
 font_add_google("Righteous", family = "clim")
 font_add_google("Montserrat", family = "mont")
+sysfonts::font_add('fb', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf')
+sysfonts::font_add('fs', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Free-Solid-900.otf')
 
 showtext_auto()
+showtext::showtext_opts(dpi = 300)
+
+here::here("GIF-5")
+
+shapefile <- st_read("Shapefiles/GR_bsm_admn_adm2_py_EuroGeoGraphics_2015_pp.shp")
 
 
-## Read shapefile
+## Download and load dataset
 
-greece_region_map = st_read("Shapefiles/Greece_Region_Map/d7f50467-e5ef-49ac-a7ce-15df3e2ed738.shp") 
+download.file(url = "https://elstat-outsourcers.statistics.gr/census_results_2022_en.pdf",
+              destfile = "GIF-5/census.pdf")
 
-## Rename regions
+download.file(url = "https://www.statistics.gr/en/statistics?p_p_id=documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_count=4&p_p_col_pos=2&_documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ_javax.faces.resource=document&_documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ_ln=downloadResources&_documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ_documentID=513865&_documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ_locale=en",
+              destfile = "GIF-5/heating_oil.xlsx")
 
-greece_region_map$PER = c("Eastern Macedonia and Thrace","Central Macedonia","Western Macedonia",
-                          "Epirus", "Thessaly", "North Aegean", "South Aegean", "Central Greece",
-                          "Western Greece", "Peloponnese", "Ionian Islands", "Crete", "Attica")
+# Extract only the pages I am interested at
 
-## Add observations
+pdf_subset("GIF-5/census.pdf",
+           pages = 6,  output = "GIF-5/subset.pdf")
 
-greece_region_map$cons_diesel_heat = c(88827.3334, 237736.297, 43483.0709, 
-                                       47700.1682, 74639.1059, 17541.408, 
-                                       15278.4606, 73956.5803, 45726.2929, 
-                                       50750.9388, 18069.8432, 34796.1136, 
-                                       290475.5617)
 
-greece_region_map$pop2021 = c(562201, 1795669, 254595,
-                              319991, 688255, 194943,
-                              327820, 508254, 648220,
-                              539535, 204532, 624408,
-                              3814064)
 
-greece_region_map$per1000 = greece_region_map$cons_diesel_heat/greece_region_map$pop2021*1000
+statistics_tables <- extract_tables(
+  file   = "GIF-5/subset.pdf", 
+  method = "decide", 
+  output = "data.frame")
 
-# Visualization
+regional_population = statistics_tables[[1]] |>
+  select(-1) |>
+  select(1,3) |>
+  setNames(c("Region", "Population")) |>
+  mutate(
+    Population = stringr::str_replace_all(Population, ",", "")
+  ) |>
+  tidyr::drop_na() |>
+  mutate(
+    Region = stringr::str_to_title(Region)
+  ) |>
+  slice(-nrow(statistics_tables[[1]]))
 
-map = ggplot2::ggplot(data = greece_region_map) +
-  geom_sf(aes(fill = per1000)) +
-  geom_sf_text(aes(label = round(per1000, digits = 0)),
-               colour = "black", 
-               size = 15,
-               family="mont") +
-  labs(
-    title = "Heating Oil Consumption",
-    subtitle = "Consumption in metric tones per thousand inhabitants (regional data, 2020)",
-    caption = "**stesiam** | Source: Ministry of Environment and Energy"
+
+# Load data
+
+heating_oil <- read_excel("GIF-5/heating_oil.xlsx", 
+                          skip = 2) |>
+  select(1,5) |>
+  setNames(c("Region", "HeatOil")) |>
+  tidyr::drop_na()
+
+nuts1 = c("Voreia Ellada", "Kentriki Ellada", "Nisia Aigaiou, Kriti")
+
+is_similar <- function(x, exclude_strings, threshold = 0.2) {
+  # Calculate the Jaro-Winkler distance for each string in exclude_strings
+  distances <- stringdist::stringdistmatrix(x, exclude_strings, method = "dl")
+  # Find the minimum distance for the closest match
+  min_distance <- min(distances)
+  # Return TRUE if the minimum distance is below the threshold, meaning it's "similar"
+  return(min_distance < threshold)
+}
+
+filter_out = c("Not Declared", 
+               "Total Of Greece", "Nisia Agaioy, Kriti"
+)
+
+# Apply the fuzzy matching function and filter out similar rows
+df_filtered <- heating_oil %>%
+  rowwise() %>%
+  filter(!is_similar(Region, nuts1, threshold = 0.1)) |>
+  dplyr::filter(! Region %in% filter_out)
+
+
+heatingOIl_pop_joined = stringdist_left_join(df_filtered, regional_population,
+                                             by = "Region", distance = "distance",
+                                             method = "lv", max_dist = 13) |>
+  dplyr::rename(NAMA2 = Region.y) |>
+  group_by(NAMA2) |>
+  dplyr::filter(distance == min(distance)) |>
+  mutate(HeatOilLitres = HeatOil * 1000 / 0.85) |>
+  mutate(HeatOilCapita = HeatOilLitres/as.numeric(Population)) |>
+  arrange(NAMA2) |>
+  select(NAMA2, HeatOilCapita)
+
+
+
+
+data_shp_joined = stringdist_left_join(heatingOIl_pop_joined, shapefile,
+                                       by = "NAMA2", distance = "distance",
+                                       method = "lv", max_dist = 8) |>
+  group_by(NAMA2.x) |>
+  dplyr::filter(distance == min(distance)) |>
+  ungroup(NAMA2.x) |>
+  rename(Region = NAMA2.x) |>
+  select(Region, HeatOilCapita, geometry) |>
+  arrange(Region) |>
+  mutate(HeatOilCapita = round(HeatOilCapita, 2)) |>
+  st_as_sf()
+
+
+## Viz texts
+
+title_text = glue("<b>Heating Oil Consumption</b>")
+subtitle_text = glue("Northern profectures of Greece have significantly higher cosumption of heating oil.
+                     An expected fact as Kastoria (northern Greece town) averages at -10C and ")
+caption_text = glue("*Data of:* 2022 - Regional Population is based on 2021 census<br><b> Source:</b> Ministry of Environment and Energy <br><span style='font-family:fb;'  >&#xf09b;</span> <b>stesiam</b>, 2023")
+
+## Viz background
+
+bg_gradient <- grid::linearGradient(colours = rev(MetBrewer::met.brewer("Cassatt2")[4:2]))
+
+map = ggplot2::ggplot(data = data_shp_joined) +
+  scale_x_continuous(limits = c(19.7, 28.3)) +
+  geom_sf(aes(fill = HeatOilCapita)) +
+  
+  # Eastern Macedonia and Thrace
+  
+  geom_sf_text(
+    x = 25.2, y = 41.15, 
+    label = data_shp_joined$HeatOilCapita[1], 
+    size = 7.5, 
+    family = "mont",
+    fontface = "bold",
+    color = "white"
   ) +
-  scale_fill_gradient(low='white', high='#ff0000') +
-  theme_void() +
+  
+  # Attica (Athens)
+  geom_sf_text(
+    x = 23.8, y = 37.96, 
+    label = data_shp_joined$HeatOilCapita[2], 
+    size = 3.4, 
+    family = "mont",
+    fontface = "bold",
+    angle = -50,
+    color = "black"
+  ) +
+  # Western Greece
+  geom_sf_text(
+    x = 21.2, y = 38.65, 
+    label = data_shp_joined$HeatOilCapita[3], 
+    size = 4.5, 
+    family = "mont",
+    fontface = "bold",
+    angle = -60
+  ) +
+  
+  # Western Macedonia
+  
+  geom_sf_text(
+    x = 21.5, y = 40.4, 
+    label = data_shp_joined$HeatOilCapita[4], 
+    size = 5, 
+    family = "mont",
+    fontface = "bold",
+    color = "white"
+  ) +
+  
+  # Ionian Islands
+  
+  geom_sf_text(
+    x = 20.2, y = 39,
+    label = data_shp_joined$HeatOilCapita[5], 
+    size = 6, 
+    family = "mont",
+    fontface = "bold",
+    angle = -60
+  ) +
+  
+  # Epirus
+  
+  geom_sf_text(
+    x = 20.75, y = 39.6,
+    label = data_shp_joined$HeatOilCapita[6], 
+    size = 5, 
+    family = "mont",
+    fontface = "bold",
+    color = "white"
+  ) +
+  
+  # Central Macedonia
+  
+  geom_sf_text(
+    x = 23, y = 40.8, 
+    label = data_shp_joined$HeatOilCapita[7], 
+    size = 8.5, 
+    family = "mont",
+    fontface = "bold",
+    color = "white"
+  ) +
+  
+  # Crete
+  
+  geom_sf_text(
+    x = 25.1, y = 35.2, 
+    label = data_shp_joined$HeatOilCapita[8], 
+    size = 5, 
+    family = "mont",
+    fontface = "bold",
+    angle = -10
+  ) +
+  
+  # South Aegean
+  
+  geom_sf_text(
+    x = 26, y = 36.8, 
+    label = data_shp_joined$HeatOilCapita[9], 
+    size = 10, 
+    family = "mont",
+    fontface = "bold",
+    angle = -10
+  ) +
+  
+  # Peloponnese
+  
+  geom_sf_text(
+    x = 22.4, y = 37.45, 
+    label = data_shp_joined$HeatOilCapita[10], 
+    size = 5.4, 
+    family = "mont",
+    fontface = "bold",
+    angle = -10
+  ) +
+  
+  # Central Greece
+  
+  geom_sf_text(
+    x = 22.65, y = 38.6, 
+    label = data_shp_joined$HeatOilCapita[11], 
+    size = 6, 
+    family = "mont",
+    fontface = "bold",
+    angle = -25
+  ) +
+  
+  # Thessaly
+  
+  geom_sf_text(
+    x = 22.15, y = 39.5, 
+    label = data_shp_joined$HeatOilCapita[12], 
+    size = 7, 
+    family = "mont",
+    fontface = "bold",
+    angle = 0
+  ) +
+  
+  # North Aegean
+  
+  geom_sf_text(
+    x = 25.9, y = 39.2, 
+    label = data_shp_joined$HeatOilCapita[13], 
+    size = 10, 
+    family = "mont",
+    fontface = "bold",
+    angle = -60
+  ) +
+  
+  labs(
+    title = title_text,
+    subtitle = subtitle_text,
+    caption = caption_text
+  ) +
+  scale_fill_gradientn(colors = met.brewer("OKeeffe2")) +
+  theme_void(base_size = 11) +
   theme(
-    plot.title = element_markdown(family = "clim", size = 60, hjust = 0.5),
-    plot.subtitle = element_markdown(family = "mont", size = 25, hjust = 0.5, lineheight = 0.5),
-    plot.caption = element_markdown(family = "mont", size = 20, hjust = 0.5),
-    legend.position = "none"
+    plot.title = element_markdown(family = "clim", hjust = 0.5, size = 20,
+                                  margin = margin(t = 20, b = 10),
+                                  color = "white"),
+    plot.subtitle = element_textbox_simple(size = 9, family = "mont", hjust = 0.5, 
+                                           lineheight = 1.2, color = "white",
+                                           margin = margin(l = 10, r = 10)),
+    plot.caption = element_markdown(family = "mont", hjust = 0.5,
+                                    margin = margin(t = 5, b = 5),
+                                    lineheight = 1.2,
+                                    color = "white",
+                                    size = 7),
+    legend.position = "none",
+    plot.background = element_rect(fill = bg_gradient, color = "transparent")
   )
 
 ggsave(
   filename = "GIF-5/GIF-5.png",
+  height = 7,
+  width = 5.165,
   plot = map,
   device = "png",
-  bg = "white")
+  dpi = 300)
--- GIF-6/GIF-6.R
+++ GIF-6/GIF-6.R
@@ -1,69 +1,346 @@
-## Libraries
+library(readr)
+library(tidyr)
+library(stringi)
+library(readxl)
+library(dplyr)
+library(fuzzyjoin)
+library(qpdf)
+library(tabulizer)
 
 library(ggplot2)
+library(MetBrewer)
 library(sf)
+library(glue)
 library(ggtext)
 library(showtext)
-library(readxl)
+library(sysfonts)
 
-## Add fonts
 
 font_add_google("Righteous", family = "clim")
 font_add_google("Montserrat", family = "mont")
+sysfonts::font_add('fb', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf')
+sysfonts::font_add('fs', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Free-Solid-900.otf')
 
 showtext_auto()
+showtext::showtext_opts(dpi = 300)
+
+transliterate_greek_to_english <- function(greek_string) {
+  
+  # Step 1: Create a named vector for removing accents from Greek letters
+  accent_removal <- c(
+    "Ά" = "Α", "ά" = "α",
+    "Έ" = "Ε", "έ" = "ε",
+    "Ή" = "Η", "ή" = "η",
+    "Ί" = "Ι", "ί" = "ι", "ΐ" = "ι", "ϊ" = "ι",
+    "Ό" = "Ο", "ό" = "ο",
+    "Ύ" = "Υ", "ύ" = "υ", "ΰ" = "υ", "ϋ" = "υ",
+    "Ώ" = "Ω", "ώ" = "ω"
+  )
+  
+  # Replace accented Greek letters with their non-accented versions
+  for (accented_letter in names(accent_removal)) {
+    greek_string <- gsub(accented_letter, accent_removal[accented_letter], greek_string)
+  }
+  
+  # Step 2: Create a named vector for exact Greek to English transliteration
+  greek_to_english <- c(
+    "Α" = "A", "α" = "a",
+    "Β" = "B", "β" = "b",
+    "Γ" = "G", "γ" = "g",
+    "Δ" = "D", "δ" = "d",
+    "Ε" = "E", "ε" = "e",
+    "Ζ" = "Z", "ζ" = "z",
+    "Η" = "I", "η" = "i",
+    "Θ" = "Th", "θ" = "th",
+    "Ι" = "I", "ι" = "i",
+    "Κ" = "K", "κ" = "k",
+    "Λ" = "L", "λ" = "l",
+    "Μ" = "M", "μ" = "m",
+    "Ν" = "N", "ν" = "n",
+    "Ξ" = "X", "ξ" = "x",
+    "Ο" = "O", "ο" = "o",
+    "Π" = "P", "π" = "p",
+    "Ρ" = "R", "ρ" = "r",
+    "Σ" = "S", "σ" = "s", "ς" = "s",
+    "Τ" = "T", "τ" = "t",
+    "Υ" = "Y", "υ" = "y",
+    "Φ" = "F", "φ" = "f",
+    "Χ" = "Ch", "χ" = "ch",
+    "Ψ" = "Ps", "ψ" = "ps",
+    "Ω" = "O", "ω" = "o"
+  )
+  
+  # Step 3: Replace each Greek letter with its English equivalent
+  for (greek_letter in names(greek_to_english)) {
+    greek_string <- gsub(greek_letter, greek_to_english[greek_letter], greek_string)
+  }
+  
+  return(greek_string)
+}
+
+here::here("GIF-6")
+
+shapefile <- st_read("Shapefiles/GR_bsm_admn_adm2_py_EuroGeoGraphics_2015_pp.shp")
 
 
-## Read shapefile
+## Download and load dataset
 
-greece_region_map = st_read("Shapefiles/Greece_Region_Map/d7f50467-e5ef-49ac-a7ce-15df3e2ed738.shp") 
+download.file("https://www.statistics.gr/en/statistics?p_p_id=documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_count=4&p_p_col_pos=2&_documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ_javax.faces.resource=document&_documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ_ln=downloadResources&_documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ_documentID=106329&_documents_WAR_publicationsportlet_INSTANCE_VBZOni0vs5VJ_locale=en",
+              destfile = "GIF-6/electricity_2012.xls")
 
-## Rename regions
+download.file(url = "https://elstat-outsourcers.statistics.gr/census_results_2022_en.pdf",
+              destfile = "GIF-6/census.pdf")
 
-greece_region_map$PER = c("Eastern Macedonia and Thrace","Central Macedonia","Western Macedonia",
-                          "Epirus", "Thessaly", "North Aegean", "South Aegean", "Central Greece",
-                          "Western Greece", "Peloponnese", "Ionian Islands", "Crete", "Attica")
-
-
-greece_region_map$elec2012 = c(843946.91, 3019191.59, 412555.30,
-                               454254.29, 1050738.12, 334497.85,
-                               555784.55, 895781.60, 1036741.80,
-                               1070718.78, 371099.24, 932433.50,
-                               7476845.69)
+pdf_subset("GIF-6/census.pdf",
+           pages = 6,  output = "GIF-6/subset.pdf")
 
 
-greece_region_map$pop2011 = c(608182, 1882108, 283689,
-                              336856, 732762, 199231,
-                              309015, 547390, 679796,
-                              577903, 207855, 623065,
-                              3828434)
+statistics_tables <- extract_tables(
+  file   = "GIF-6/subset.pdf", 
+  method = "decide", 
+  output = "data.frame")
 
-greece_region_map$per1000 = greece_region_map$elec2012/greece_region_map$pop2011*1000
+regional_population = statistics_tables[[1]] |>
+  select(-1) |>
+  select(1,3) |>
+  setNames(c("Region", "Population")) |>
+  mutate(
+    Population = stringr::str_replace_all(Population, ",", "")
+  ) |>
+  tidyr::drop_na() |>
+  mutate(
+    Region = stringr::str_to_title(Region)
+  ) |>
+  slice(-nrow(statistics_tables[[1]]))
 
-# Visualization
 
-map = ggplot2::ggplot(data = greece_region_map) +
-  geom_sf(aes(fill = per1000)) +
-  geom_sf_text(aes(label = round(per1000, digits = 0)),
-               colour = "black", 
-               size = 15,
-               family="mont") +
-  labs(
-    title = "Electricity Consumption",
-    subtitle = "Consumption in thousands kwh per thousand inhabitants (regional data, 2012)",
-    caption = "**stesiam** | Source: Hellenic Statistical Authority"
+
+electricity_consumption_raw = read_excel("GIF-6/electricity_2012.xls", 
+                                                             skip = 4) |>
+  select(c(1,3, last_col())) |>
+  setNames(c("RegionGR", "Electricity", "RegionEN")) |>
+  drop_na() %>%
+  mutate(
+    Region = transliterate_greek_to_english(RegionGR)
+  ) |>
+  select(Region, Electricity)
+
+
+
+# Join electricity and regional population datasets
+
+joined_data = stringdist_left_join(regional_population, electricity_consumption_raw, 
+                     by = "Region", 
+                     distance_col = "distance",
+                     max_dist = 10) |>
+  group_by(Region.x) |>
+  dplyr::filter(distance == min(distance)) |>
+  select(Region.x, Electricity, Population) |>
+  rename(NAMA2 = Region.x) |>
+  mutate(ElectricityCapita = round(1000*Electricity/as.numeric(Population), digits = 0)) |>
+  distinct(NAMA2, .keep_all = TRUE)
+
+
+#
+
+joined_data_shp = stringdist_left_join(shapefile, joined_data, 
+                                  by = "NAMA2", 
+                                  distance_col = "distance",
+                                  max_dist = 10) |>
+  group_by(NAMA2.x) |>
+  dplyr::filter(distance == min(distance)) |>
+  distinct(NAMA2.x,.keep_all = TRUE) |>
+  dplyr::filter(NAMA2.x != "Agion Oros") |>
+  select(NAMA2.x, ElectricityCapita, geometry) |>
+  rename(Region = NAMA2.x) |>
+  arrange(Region) |>
+  st_as_sf()
+
+
+## Viz texts
+
+title_text = glue("<b>Electricity Consumption</b>")
+subtitle_text = glue("Electric energy consumption by great geographic area, region and department and by category of use:2012")
+caption_text = glue("*Data of:* 2012 - Regional Population is based on 2021 census<br><b> Source:</b> Hellenic Statistical Authority<br><span style='font-family:fb;'  >&#xf09b;</span> <b>stesiam</b>, 2023")
+
+## Viz background
+
+bg_gradient <- grid::linearGradient(colours = rev(MetBrewer::met.brewer("Morgenstern")[7:8]))
+
+## Make map
+
+map = ggplot2::ggplot(data = joined_data_shp) +
+  scale_x_continuous(limits = c(19.7, 28.3)) +
+  geom_sf(aes(fill = ElectricityCapita)) +
+  
+  # Eastern Macedonia and Thrace
+  
+  geom_sf_text(
+    x = 25.2, y = 41.15, 
+    label = joined_data_shp$ElectricityCapita[1], 
+    size = 7.5, 
+    family = "mont",
+    fontface = "bold",
+    color = "black"
   ) +
-  scale_fill_gradient(low='white', high='#e5de00') +
-  theme_void() +
+  
+  # Attica (Athens)
+  geom_sf_text(
+    x = 23.8, y = 37.96, 
+    label = joined_data_shp$ElectricityCapita[2], 
+    size = 3.4, 
+    family = "mont",
+    fontface = "bold",
+    angle = -50,
+  ) +
+  # Western Greece
+  geom_sf_text(
+    x = 21.2, y = 38.65, 
+    label = joined_data_shp$ElectricityCapita[3], 
+    size = 4.5, 
+    family = "mont",
+    fontface = "bold",
+    angle = -60
+  ) +
+  
+  # Western Macedonia
+  
+  geom_sf_text(
+    x = 21.5, y = 40.4, 
+    label = joined_data_shp$ElectricityCapita[4], 
+    size = 5, 
+    family = "mont",
+    fontface = "bold",
+    color = "black"
+  ) +
+  
+  # Ionian Islands
+  
+  geom_sf_text(
+    x = 20.2, y = 39,
+    label = joined_data_shp$ElectricityCapita[5], 
+    size = 6, 
+    family = "mont",
+    fontface = "bold",
+    angle = -60
+  ) +
+  
+  # Epirus
+  
+  geom_sf_text(
+    x = 20.75, y = 39.6,
+    label = joined_data_shp$ElectricityCapita[6], 
+    size = 5, 
+    family = "mont",
+    fontface = "bold",
+    color = "black"
+  ) +
+  
+  # Central Macedonia
+  
+  geom_sf_text(
+    x = 23, y = 40.8, 
+    label = joined_data_shp$ElectricityCapita[7], 
+    size = 8.5, 
+    family = "mont",
+    fontface = "bold",
+    color = "black"
+  ) +
+  
+  # Crete
+  
+  geom_sf_text(
+    x = 25.1, y = 35.2, 
+    label = joined_data_shp$ElectricityCapita[8], 
+    size = 5, 
+    family = "mont",
+    fontface = "bold",
+    angle = -10
+  ) +
+  
+  # South Aegean
+  
+  geom_sf_text(
+    x = 26, y = 36.8, 
+    label = joined_data_shp$ElectricityCapita[9], 
+    size = 10, 
+    family = "mont",
+    fontface = "bold",
+    angle = -10
+  ) +
+  
+  # Peloponnese
+  
+  geom_sf_text(
+    x = 22.4, y = 37.45, 
+    label = joined_data_shp$ElectricityCapita[10], 
+    size = 5.4, 
+    family = "mont",
+    fontface = "bold",
+    angle = -10,
+    color = "black"
+  ) +
+  
+  # Central Greece
+  
+  geom_sf_text(
+    x = 22.65, y = 38.6, 
+    label = joined_data_shp$ElectricityCapita[11], 
+    size = 6, 
+    family = "mont",
+    fontface = "bold",
+    angle = -25
+  ) +
+  
+  # Thessaly
+  
+  geom_sf_text(
+    x = 22.15, y = 39.5, 
+    label = joined_data_shp$ElectricityCapita[12], 
+    size = 7, 
+    family = "mont",
+    fontface = "bold",
+    angle = 0
+  ) +
+  
+  # North Aegean
+  
+  geom_sf_text(
+    x = 25.9, y = 39.2, 
+    label = joined_data_shp$ElectricityCapita[13], 
+    size = 10, 
+    family = "mont",
+    fontface = "bold",
+    angle = -60
+  ) +
+  
+  labs(
+    title = title_text,
+    subtitle = subtitle_text,
+    caption = caption_text
+  ) +
+  scale_fill_gradient(low = "grey90", high = "yellow2") +
+  theme_void(base_size = 11) +
   theme(
-    plot.title = element_markdown(family = "clim", size = 60, hjust = 0.5),
-    plot.subtitle = element_markdown(family = "mont", size = 25, hjust = 0.5, lineheight = 0.5),
-    plot.caption = element_markdown(family = "mont", size = 20, hjust = 0.5),
-    legend.position = "none"
+    plot.title = element_markdown(family = "clim", hjust = 0.5, size = 20,
+                                  margin = margin(t = 20, b = 10),
+                                  color = "white"),
+    plot.subtitle = element_textbox_simple(size = 9, family = "mont", hjust = 0.5, 
+                                           lineheight = 1.2, color = "white",
+                                           margin = margin(l = 10, r = 10)),
+    plot.caption = element_markdown(family = "mont", hjust = 0.5,
+                                    margin = margin(t = 5, b = 5),
+                                    lineheight = 1.2,
+                                    color = "white",
+                                    size = 7),
+    legend.position = "none",
+    plot.background = element_rect(fill = bg_gradient, color = "transparent")
   )
 
 ggsave(
   filename = "GIF-6/GIF-6.png",
+  height = 7,
+  width = 5.46,
   plot = map,
   device = "png",
-  bg = "white")
+  dpi = 300)
